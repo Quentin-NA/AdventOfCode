91fd813c1e1b14b47516b46fee426a6eb4e64641
Quentin-NA <quentin.natoly@gmail.com>
2023-12-03 13:55:21 +0100

    WIP

--- day1/trebuchet.js
+++ day1/trebuchet.js
@@ -14,4 +14,64 @@ const partOne = () => {
         return values.reduce((s, v) => s + v)
 }
 
-console.log(partOne())
+// console.log(partOne())
+
+
+const firstNumberWordsRegExp = new RegExp (["one", "two", "three", "four", "five", "six", "seven", "height", "nine", "ten"].join('|'))
+const lastNumberWordsRegExp = new RegExp (["one", "two", "three", "four", "five", "six", "seven", "height", "nine", "ten"].join('|').split('').reverse().join(''))
+
+wordMap =  {
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    height: "8",
+    nine: "9",
+    ten: "10" 
+}
+
+
+const partTwo = () => {
+    const values = lines
+        .map((line) => {
+            let firstNumberIndex = line
+                .split('')
+                .find((v) => !Number.isNaN(Number(v)))
+            let firstWordMatch = line
+            .split('')
+            .join('')
+            .match(firstNumberWordsRegExp)
+
+            let firstWordNumberIndex = firstWordMatch?.index;
+
+            let firstNumber = firstWordMatch ? (
+                firstNumberIndex < firstWordNumberIndex ?
+                line[firstNumberIndex] : wordMap[firstWordMatch[0]]
+            ) : firstNumberIndex
+
+
+            let lastNumberIndex = line
+                .split('')
+                .find((v) => !Number.isNaN(Number(v)))
+            let lastWordMatch = line
+            .split('')
+            .reverse()
+            .join('')
+            .match(lastNumberWordsRegExp)
+
+            let lastWordNumberIndex = lastWordMatch?.index;
+
+            let lastNumber = lastWordMatch ? (
+                lastNumberIndex < lastWordNumberIndex ?
+                line[lastNumberIndex] : wordMap[lastWordMatch[0].split('').reverse().join("")]
+            ) : lastNumberIndex
+
+            console.log(firstNumber, lastNumber)
+            return Number(firstNumber + lastNumber)
+        })
+}
+
+console.log(partTwo())
