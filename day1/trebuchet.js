c28326bacd48a343d72382fca93ce894a9b4491d
Quentin-NA <quentin.natoly@gmail.com>
2023-12-03 14:41:31 +0100

    PartTwo function working

--- day1/trebuchet.js
+++ day1/trebuchet.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 
-const content = fs.readFileSync('exemples.txt', 'utf-8').trim();
-const lines = content.split('\n').map(line => line.replace('\r', ''));
 
 const partOne = () => {
     const values = lines
@@ -17,8 +15,8 @@ const partOne = () => {
 // console.log(partOne())
 
 
-const firstNumberWordsRegExp = new RegExp (["one", "two", "three", "four", "five", "six", "seven", "height", "nine", "ten"].join('|'))
-const lastNumberWordsRegExp = new RegExp (["one", "two", "three", "four", "five", "six", "seven", "height", "nine", "ten"].join('|').split('').reverse().join(''))
+const firstNumberWordsRegExp = new RegExp (["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].join('|'))
+const lastNumberWordsRegExp = new RegExp (["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].join('|').split('').reverse().join(''))
 
 wordMap =  {
     one: "1",
@@ -28,50 +26,58 @@ wordMap =  {
     five: "5",
     six: "6",
     seven: "7",
-    height: "8",
-    nine: "9",
-    ten: "10" 
+    eight: "8",
+    nine: "9" 
 }
 
 
 const partTwo = () => {
+    const content = fs.readFileSync('exemples.txt', 'utf-8').trim();
+    const lines = content.split('\n').map(line => line.replace('\r', ''));
+
     const values = lines
         .map((line) => {
             let firstNumberIndex = line
                 .split('')
-                .find((v) => !Number.isNaN(Number(v)))
-            let firstWordMatch = line
-            .split('')
-            .join('')
-            .match(firstNumberWordsRegExp)
+                .findIndex((v) => !Number.isNaN(Number(v)))
+            let firstWordMatch = line.match(firstNumberWordsRegExp)
 
             let firstWordNumberIndex = firstWordMatch?.index;
 
-            let firstNumber = firstWordMatch ? (
-                firstNumberIndex < firstWordNumberIndex ?
-                line[firstNumberIndex] : wordMap[firstWordMatch[0]]
-            ) : firstNumberIndex
-
+            let firstNumber = 
+                firstNumberIndex != -1
+                    ? firstWordMatch
+                        ? firstNumberIndex < firstWordNumberIndex
+                            ? line[firstNumberIndex]
+                            : wordMap[firstWordMatch[0]]
+                        :line[firstNumberIndex]
+                    :wordMap[firstWordMatch[0]];
 
             let lastNumberIndex = line
                 .split('')
-                .find((v) => !Number.isNaN(Number(v)))
+                .findLastIndex((v) => !Number.isNaN(Number(v)))
             let lastWordMatch = line
             .split('')
             .reverse()
             .join('')
             .match(lastNumberWordsRegExp)
 
-            let lastWordNumberIndex = lastWordMatch?.index;
+            let lastWordNumberIndex = lastWordMatch ? line.length -1 - lastWordMatch.index : null;
 
-            let lastNumber = lastWordMatch ? (
-                lastNumberIndex < lastWordNumberIndex ?
-                line[lastNumberIndex] : wordMap[lastWordMatch[0].split('').reverse().join("")]
-            ) : lastNumberIndex
+            let lastNumber = 
+                lastNumberIndex != -1
+                    ? lastWordMatch
+                        ? lastNumberIndex > lastWordNumberIndex
+                            ? line[lastNumberIndex]
+                            : wordMap[lastWordMatch[0].split('').reverse().join('')]
+                        :line[lastNumberIndex]
+                    :wordMap[lastWordMatch[0].split('').reverse().join('')];
 
-            console.log(firstNumber, lastNumber)
             return Number(firstNumber + lastNumber)
         })
+
+        console.log(values.reduce((s, v) => s + v))
+        return values.reduce((s, v) => s + v)
 }
 
-console.log(partTwo())
+partTwo()
