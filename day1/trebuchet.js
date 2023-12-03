3646c68d5ce4e53790cf446ca7ead6d409bb3452
Quentin-NA <quentin.natoly@gmail.com>
2023-12-03 12:48:23 +0100

    partOne function ok => get first star

--- day1/trebuchet.js
+++ day1/trebuchet.js
@@ -3,4 +3,15 @@ const fs = require('fs');
 const content = fs.readFileSync('exemples.txt', 'utf-8').trim();
 const lines = content.split('\n').map(line => line.replace('\r', ''));
 
-console.log(lines);
+const partOne = () => {
+    const values = lines
+        .map((line) => {
+            const first = line.split('').find((v) => !Number.isNaN(Number(v)))
+            const last = line.split('').findLast((v) => !Number.isNaN(Number(v)))
+            return Number(first + last)
+        })
+
+        return values.reduce((s, v) => s + v)
+}
+
+console.log(partOne())
